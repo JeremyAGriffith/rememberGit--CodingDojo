26093a638a4a4454202dbf21c623ec92ab4d80c2
JeremyAGriffith <arthur.beardsly@gmail.com>
2013-07-25 14:25:05 -0700

    I have added a list of links and a contact page

--- contact.php
+++ contact.php
@@ -2,15 +2,15 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>source control - Remembering-Git</title>
+	<title>source control - Remembering-Git CONTACT</title>
 </head>
 <body>
-	<h1>Hello World!</h1>
+	<h1>I'm a test contact page</h1>
 	<ul>
 		<li><a href="index.php">home</a></li>
 		<li><a href="contact.php">contact</a></li>
 		<li><a href="#">another link</a></li>
 	</ul>
-	<h2>this is my second change...</h2>
+	<h2>contact me at ###.###.####</h2>
 </body>
 </html>
